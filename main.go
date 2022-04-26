62b66cdceee13f0c8c5efba95b52fa43c1be3668
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-04-26 12:41:34 +0530

    feat(go): logger

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
+	f "fmt"
 	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	l "github.com/rs/zerolog/log"
 )
 
 func main() {
-	port := 3000
-	fmt.Println(fmt.Sprintf("Starting Server on Port: %d", port))
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	l.Logger = l.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	port := 8080
+	l.Debug().Msgf("Starting Server on Port: %d", port)
 
 	http.HandleFunc("/", routes)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(f.Sprintf("localhost:%d", port), nil)
+	if err != nil {
+		l.Err(err)
+		return
+	}
 }
 
-func routes(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World"))
+func routes(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Hello, World"))
+	if err != nil {
+		l.Err(err)
+		return
+	}
 }
