28d12902209b35d791b8954f5c59619e23cc28e8
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-04-27 14:21:52 +0530

    feat(go): script

---
+++ abi_to_go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -n "Enter path to ABI/JSON: "
+read file_path
+echo -n "Enter Contract Name: "
+read abi_name
+echo "⏳ Loading $abi_name ..."
+
+mkdir -p ./abis/$abi_name
+abigen --pkg main --abi $file_path --out ./abis/$abi_name/$abi_name.go
+
+if [ $? -eq 0 ]; then
+    echo "✅ $abi_name loaded successfully"
+else
+    rm -rf ./abis/$abi_name
+    echo "❌ $abi_name failed to load"
+fi
